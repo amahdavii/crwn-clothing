a7b3731b592bf78d149d62fdf44e70504a5d43ce
Arian Mahdavi <amahdavi9978@gmail.com>
2021-05-10 11:19:47 +0430

    Tweak Styling.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom';
 
 import HomePage from './Pages/Homepage/Homepage.component';
 import ShopPage from './Pages/Shop/Shop.component';
+import SignInAndSignUpPage from './Pages/Sign-in-and-Sign-up/Sign-in-and-Sign-up.component';
 import Header from './Components/Header/Header.component';
 
 import './App.css';
@@ -14,6 +15,7 @@ function App() {
       <Header />
       <Route exact path="/" component={HomePage} />
       <Route path="/shop" component={ShopPage} />
+      <Route path="/signin" component={SignInAndSignUpPage} />
     </div>
   );
 }
---
+++ src/Pages/Sign-in-and-Sign-up/Sign-in-and-Sign-up.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Sign-in-and-Sign-up.styles.scss';
+
+const SignInAndSignUpPage = () => (
+    <div className="sign-in-and-sign-up">
+        Sign In
+    </div>
+);
+ 
+export default SignInAndSignUpPage;
